406289fd607ead99e852387ad3137a842cb0e3d2
Felipe Horizonte <felipe.hrtz2@gmail.com>
2021-09-06 19:55:24 -0300

    adicionando com sucesso

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const config = require('./config.json');
 const client = new Discord.Client();
-const { main } = require('./mongodb.js');
+const { main, getUsuarios, insertUsuarioCollection } = require('./mongodb.js');
 
 const cargos = {
   "Membro Avanti": {},
@@ -13,11 +13,28 @@ const cargos = {
   "Santista": {}
 }
 
+client.on("guildMemberAdd", member => {
+  
+});
 
-
-client.on('ready', function (message) {
+client.on('ready', async function () {
     const server = client.guilds.cache.get('884519606218293310'); //servidor
-    main().catch(console.error);
+  
+    main(async client => {
+      let usuariosDB = await getUsuarios(client);
+
+      server.members.cache.forEach(member => {
+        usuariosDB.forEach(usuario => {
+          if(member.user.id !== usuario.id) {
+            main(client => insertUsuarioCollection(client, {
+              name: member.user.username,
+              id: member.user.id,
+            }))
+          }
+        })
+      });
+    })
+
 });
 
 client.on("message", function (message) {
--- mongodb.js
+++ mongodb.js
@@ -6,13 +6,31 @@ async function listDatabases(client) {
     databasesList.databases.forEach(db => console.log(` - ${db.name}`));
 }
 
+async function insertUsuarioCollection(client, usuario) {
+    const database = client.db("Felipao");
+    const usuarios = database.collection("usuarios");
 
-async function main() {
+    const usuario_template = {
+        name: usuario.name,
+        id: usuario.id,
+    }
+    const result = await usuarios.insertOne(usuario_template);
+    console.log(`${result.insertCount} usuarios foram adicionados`);
+}  
+
+async function getUsuarios(client) {
+    const database = client.db("Felipao");
+    const usuarios = await database.collection("usuarios").find({}).toArray();
+
+    return usuarios;
+}
+
+async function main(callback) {
     const uri = "mongodb+srv://admin:rpU9FsfI1cIcz3Qv@cluster0.xvvlz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
     const client = new MongoClient(uri);
     try {
         await client.connect();
-        await listDatabases(client);
+        await callback(client)
     } catch (e) {
         console.log(e);
     } finally {
@@ -20,4 +38,4 @@ async function main() {
     }
 }
 
-module.exports = { main }
+module.exports = { main, listDatabases, getUsuarios, insertUsuarioCollection }
